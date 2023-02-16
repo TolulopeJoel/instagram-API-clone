0f3c800eaec4e710165bc1b4156c5666e4256f98
Tolu Joel <toluisjoel@gmail.com>
2023-02-16 14:50:29 +0100

    Made user data more accessible by adding UserPublicSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,7 +3,13 @@ from rest_framework import serializers
 from .models import Post
 
 
+class UserPublicSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
+
+
 class PostSerializer(serializers.ModelSerializer):
+    user = UserPublicSerializer(read_only=True)
     url = serializers.HyperlinkedIdentityField(
         view_name='posts-detail',
         lookup_field ='pk',
