f029908dc8c8e9703d06b91f0a2e2bdc595473e8
Tolu Joel <toluisjoel@gmail.com>
2023-02-20 15:40:06 +0100

    Added post method to comment ListCreateAPIView - enables user to be able to create comment

--- comments/views.py
+++ comments/views.py
@@ -1,12 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from posts.models import Post
 
 from .models import Comment
 from .serializers import CommentDetailSerializer, CommentListSerializer
 
 
-class CommentListView(generics.ListAPIView):
+class CommentListView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentListSerializer
+    
+    def post(self, request, *args, **kwargs):
+        post_id = request.data.get('post_id')
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return Response({'detail': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        content = request.data.get('content')
+        comment = Comment(user=request.user, post=post, content=content)
+        comment.save()
+
+        return Response(CommentListSerializer(comment).data, status=status.HTTP_201_CREATED)
 
 
 class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
